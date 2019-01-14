9e6e8107a1fa341c85fb4576f9598f902523a82d
Nadja Kelly <nmfort-info@yahoo.de>
2019-01-14 09:30:47 +0000

    finished food pantry hw

---
+++ js/apps.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+const form = document.querySelector('#new-food-items');
+form.addEventListener('submit', handleFormSubmit);
+
+});
+
+//form will empty and can be filled out again
+  const handleFormSubmit = function (event) {
+    event.preventDefault();
+    console.log(event);
+
+
+//get hold of ul in html
+const list = document.querySelector('ul')
+
+//create purchase date list and add
+const listItem = document.createElement('li')
+listItem.textContent = `${event.target.purchase.value}`;
+list.appendChild(listItem)
+
+//create item purchased list and add
+const listItem2 = document.createElement('li')
+listItem2.textContent = `${event.target.foodPurchased.value}`;
+list.appendChild(listItem2)
+
+//create quantity purchased list and add
+const listItem3 = document.createElement('li')
+listItem3.textContent = `${event.target.quantity.value}`;
+list.appendChild(listItem3)
+
+//create use by date list and add
+const listItem4 = document.createElement('li')
+listItem4.textContent = `${event.target.useByDate.value}`;
+list.appendChild(listItem4)
+
+//create select list and add
+const listItem5 = document.createElement('li')
+listItem5.textContent = `${event.target.category.value}`;
+list.appendChild(listItem5)
+
+document.getElementById('new-food-items').reset();
+
+const handleDeleteAllClick = function (event) {
+  const list = document.querySelector('#food-supplies');
+  list.innerHTML = '';
+}
+}
